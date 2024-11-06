b2b23c28aecf4ca049bab87a5d5ff3b20aeb1216
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2024-11-06 16:32:57 +0000

    Add sprite texture path

--- sprite.h
+++ sprite.h
@@ -8,6 +8,7 @@
 class Sprite {
   private:
     //Sprite Attributes
+    std::string m_texturePath{ "path/to/texture.bmp" };
     int m_width{ 0 }, m_height{ 0 };
     Point2d m_location{ 0, 0 };
     RGB m_transparency{ 255, 255, 255 };
@@ -18,12 +19,15 @@ class Sprite {
     const int getHeight() { return m_height; }
     const Point2d getLocation() { return m_location; }
     const std::string getTransparency() { return RGBtoHex(m_transparency); }
+    const std::string getTexturePath() { return m_texturePath; }
 
+    //Print the object to the console
     friend std::ostream& operator<<(std::ostream& out, Sprite& sprite) {
       out << "Sprite Object:\n";
+      out << " -Texture Path: '" << sprite.getTexturePath() << "'\n";
       out << " -Dimensions: " << sprite.getWidth() << "w x " << sprite.getHeight() << "h\n";
       out << " -Location: " << sprite.getLocation() << '\n';
-      out << " -Transparency: " << sprite.getTransparency() << "  " << hexToRGB(sprite.getTransparency());
+      out << " -Transparency: " << sprite.getTransparency() << " | " << hexToRGB(sprite.getTransparency());
       return out;
     }
 };
